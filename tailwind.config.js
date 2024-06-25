cd6d748dae7efd694a2247c74e12d76a58b639d2
zhyck1316 <zhyck1316@gmail.com>
2024-06-26 02:38:13 +0800

    Redone the Pricing section and added Pricing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./index.html", "./coverage.html", "./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      color: {
+        'dcpn': '#302f2f',
+      }
+    },
   },
   plugins: [],
 }
